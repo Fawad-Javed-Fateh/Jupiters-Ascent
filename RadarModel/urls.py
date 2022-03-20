e7954aa81d88559b77808514636c48329265ef04
Fawad-Javed-Fateh <64371800+Fawad-Javed-Fateh@users.noreply.github.com>
2022-03-20 11:13:31 +0500

    Project Setup initiated

--- RadarModel/urls.py
+++ RadarModel/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from firstPage import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('scoreJson',csrf_exempt(views.scoreJSON),name='Score Application'),
+    path('scoreFile',csrf_exempt(views.scoreFile),name='Score File'),
 ]
--- firstPage/views.py
+++ firstPage/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def scoreJSON(request):
+    return JsonResponse({'score':1})
+
+def scoreFile(request):
+    return JsonResponse({'score':1})

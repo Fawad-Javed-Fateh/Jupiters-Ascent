95321e63a9bb6e91bf5daf2611c4baad18095cfc
Fawad-Javed-Fateh <64371800+Fawad-Javed-Fateh@users.noreply.github.com>
2022-03-20 13:37:45 +0500

    Handeled initial post requests

--- firstPage/views.py
+++ firstPage/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+import joblib
+import json
+import pandas as pd 
+model=joblib.load('modelPipeline.pkl')
 # Create your views here.
 
 def scoreJSON(request):
-    return JsonResponse({'score':1})
+    print(request.body)
+    data=json.load(request.body)
+    dataFrame=pd.DataFrame({'x':data}).transpose()
+    score=model.predict_probab(dataFrame)[:,-1][0]
+    score=float(score)
+    print(score)
+    return JsonResponse({'score':score})
 
 def scoreFile(request):
     return JsonResponse({'score':1})

1f5e24004c491538e4438286857b07f07830b6fb
Fawad-Javed-Fateh <64371800+Fawad-Javed-Fateh@users.noreply.github.com>
2022-03-21 00:08:59 +0500

    Home Page Done

---
+++ frontend-ui/src/Home.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <p>Hello home </p>
+    </div>
+  );
+}
+
+export default Home;

fa0f6be583c24bdcceef8b7898dab12dc74738fc
Fawad-Javed-Fateh <64371800+Fawad-Javed-Fateh@users.noreply.github.com>
2022-03-22 00:17:24 +0500

    hopefully thigns are back on track

--- frontend-ui/src/App.js
+++ frontend-ui/src/App.js
@@ -2,13 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import UploadForm  from './FileUpload';
 import Home from './Home';
-import {BrowserRouter  as Router,Route,Switch} from 'react-router-dom'
-
+import {BrowserRouter  as Router,Route,Routes} from 'react-router-dom'
+import Footer from './footer';
+import Navbar from './navbar';
+import Singleuser from './singleuser';
 function App() {
   return (
     <div className="App">
-      <UploadForm/>
-      <Home/>
+     <Router>
+       <Routes>
+         <Route path='/' element={<Home />}/>
+         <Route path='/singleuser' element={<Singleuser/>}/>
+       </Routes>
+    <Footer/>
+     </Router>
+     
     </div>
   );
 }
